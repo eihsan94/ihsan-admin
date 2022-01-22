bf3fc82e910a7d2bebe77365a60db77cb2d85439
Engku Ihsan <eihsan94@gmail.com>
2022-01-22 20:16:58 +0900

    fix ssr auth

--- packages/frontend/utils/ssrAuth.ts
+++ packages/frontend/utils/ssrAuth.ts
@@ -1,35 +1,35 @@
 import { getSession } from "next-auth/react"
 
-const redirectHome = async(context: any, otherProps?: any) => {
-    const session = await getSession({req: context.req})
-    return session 
+const redirectHome = async (context: any, otherProps?: any) => {
+    const session = await getSession({ req: context.req })
+    return session
         ? {
-            redirect: {
-            destination:'/',
-            permanent: false,
-            }
+            props: { session }
         }
         : {
-            props: {session}
+            redirect: {
+                destination: '/',
+                permanent: false,
+            }
         }
 }
 
-const redirectAuth = async(context: any, otherProps?: any) => {
-    const session = await getSession({req: context.req})
-    const props = {session, ...otherProps}
-    
-    return session 
-        ? {props}
+const redirectAuth = async (context: any, otherProps?: any) => {
+    const session = await getSession({ req: context.req })
+    const props = { session, ...otherProps }
+
+    return session
+        ? { props }
         : {
             redirect: {
-            destination:'/auth',
-            permanent: false,
+                destination: '/auth',
+                permanent: false,
             },
         }
 }
 
 
-export  {
+export {
     redirectAuth,
     redirectHome,
 }
